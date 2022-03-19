19eb67bab2a2c53f5bb1d65a59d533c6847a910e
BobCode854 <karansinha854@gmail.com>
2022-03-19 18:59:50 +0530

    Spring boot First Project

---
+++ Spring-Without-Dependency-Injection/src/main/java/com/example/SpringWithoutDependencyInjection/SpringWithoutDependencyInjectionApplication.java
@@ -0,0 +1,15 @@
+package com.example.SpringWithoutDependencyInjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithoutDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithoutDependencyInjectionApplication.class, args);
+		System.out.println("hiiiii");
+		
+	}
+
+}
---
+++ Spring-Without-Dependency-Injection/src/test/java/com/example/SpringWithoutDependencyInjection/SpringWithoutDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringWithoutDependencyInjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithoutDependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
